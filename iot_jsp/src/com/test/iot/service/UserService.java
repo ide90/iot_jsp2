7997ab6c1ecf095763c380d3bebc192fd66c6450
ide90 <33861610+ide90@users.noreply.github.com>
2017-12-27 04:26:36 +0900

    Update UserService.java

--- iot_jsp/src/com/test/iot/service/UserService.java
+++ iot_jsp/src/com/test/iot/service/UserService.java
@@ -2,10 +2,12 @@ package com.test.iot.service;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 public interface UserService {
-	
-	public ArrayList<HashMap<String,Object>> getUserList();
-	public HashMap<String,Object>getUser();
 
+	public ArrayList<HashMap<String,Object>> getUserList();
+	public HashMap<String,Object> getUser();
+	public int executeUpdate(int i,LinkedHashMap<String,Object> hm);
+	public String mkSql(int i);
 }
