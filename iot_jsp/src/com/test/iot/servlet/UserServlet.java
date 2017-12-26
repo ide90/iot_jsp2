336a1b14858f3e91e22b2d69066e2eaf41946a08
ide90 <33861610+ide90@users.noreply.github.com>
2017-12-27 04:28:08 +0900

    Update UserServlet.java

--- iot_jsp/src/com/test/iot/servlet/UserServlet.java
+++ iot_jsp/src/com/test/iot/servlet/UserServlet.java
@@ -1,7 +1,7 @@
 package com.test.iot.servlet;
 
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -13,21 +13,22 @@ import com.test.iot.service.UserService;
 import com.test.iot.service.impl.UserServiceImpl;
 
 public class UserServlet extends HttpServlet {
+
 	UserService us = new UserServiceImpl();
 
 	public String getCommand(String uri) {
 		int idx = uri.lastIndexOf("/");
 		if (idx != -1) {
-			return uri.substring(idx+1);
+			return uri.substring(idx + 1);
 		}
 		return "";
-
 	}
 
 	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 		doProcess(req, res);
 	}
+
 	@Override
 	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 		doProcess(req, res);
@@ -39,17 +40,33 @@ public class UserServlet extends HttpServlet {
 		res.setContentType("text/html;charset=utf-8");
 		String uri = req.getRequestURI();
 		String cmd = getCommand(uri);
-		System.out.println("cmd : " + cmd);
 		if (cmd.equals("list")) {
-			req.setAttribute("list",us.getUserList());
-		} else if (cmd.equals("view")) {
-			//us.getUser();
+			req.setAttribute("list", us.getUserList());
+		} else if (cmd.equals("insert")) {
+			int i = 1;
+			LinkedHashMap<String, Object> hm = new LinkedHashMap<String, Object>();
+			hm.put("UINAME", "홍길동");
+			hm.put("UIAGE", 33);
+			hm.put("UIID", "hoong");
+			hm.put("UIPWD", "hoong");
+			hm.put("CINO", 3);
+			hm.put("address", "서울");
+			req.setAttribute("insert", us.executeUpdate(i, hm));
+		} else if (cmd.equals("delete")) {
+			int i = 2;
+			LinkedHashMap<String, Object> hm = new LinkedHashMap<String, Object>();
+			hm.put("uino", 3);
+			req.setAttribute("delete", us.executeUpdate(i, hm));
+		} else if (cmd.equals("update")) {
+			int i = 3;
+			LinkedHashMap<String, Object> hm = new LinkedHashMap<String, Object>();
+			hm.put("address", "부산");
+			hm.put("uino", 5);
+			req.setAttribute("update", us.executeUpdate(i, hm));
 		} else {
-			cmd = "/common/error";
+			cmd = "/WEB-INF/view/common/error";
 		}
-		uri ="/WEB-INF/view"+ uri +".jsp";
+		uri = "/WEB-INF/view" + uri + ".jsp";
 		RequestDispatcher rd = req.getRequestDispatcher(uri);
 		rd.forward(req, res);
-
 	}
-}
