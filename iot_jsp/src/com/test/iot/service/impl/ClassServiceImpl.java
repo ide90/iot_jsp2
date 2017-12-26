72da8d1c7f861ccd8444aa5d28745da201168a98
ide90 <33861610+ide90@users.noreply.github.com>
2017-12-27 04:23:32 +0900

    Create ClassServiceImpl.java

---
+++ iot_jsp/src/com/test/iot/service/impl/ClassServiceImpl.java
@@ -0,0 +1,57 @@
+package com.test.iot.service.impl;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+import com.test.iot.common.DBCon;
+import com.test.iot.dao.UserDAO;
+import com.test.iot.dao.impl.UserDAOImpl;
+import com.test.iot.service.ClassService;
+import com.test.iot.service.UserService;
+
+public class ClassServiceImpl implements ClassService {
+	Connection con;
+	private UserDAO ud = new UserDAOImpl();
+
+	@Override
+	public ArrayList<HashMap<String, Object>> getUserList() {
+		con = DBCon.getCon();
+		ArrayList<HashMap<String, Object>> userList = ud.selectUserList();
+
+		DBCon.closeCon();
+		return userList;
+	}
+
+	@Override
+	public HashMap<String, Object> getUser() {
+		return null;
+	}
+
+	@Override
+	public int executeUpdate(int i, LinkedHashMap<String, Object> hm) {
+		int result = 0;
+		String sql = mkSql(i);
+		result = ud.executeUpdate(sql, hm);
+
+		return result;
+	}
+
+	public String mkSql(int i) {
+		String sql = "";
+		if (i == 1) {// insert
+			sql = "INSERT INTO CLASS_INFO(CINAME,CIDESC) "
+					+ "VALUES (?,?)";
+		} else if (i == 2) {// delete
+			sql = "delete from user_info where CINO=?";
+		} else if (i == 3) {// update
+			sql = "update user_info set CIDESC=? where CINO=?";
+		} else {
+			System.out.println("에러가 발생했습니다");
+		}
+
+		return sql;
+	}
+
+}
